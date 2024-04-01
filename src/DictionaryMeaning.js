76330be5a8580685cade277649b42994cb2faf31
Sergey-Ivanovich <ericnoata@gmail.com>
2024-04-01 08:07:17 +0100

    simplified css and definition response (for now)

--- src/DictionaryMeaning.js
+++ src/DictionaryMeaning.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function DictionaryMeaning(props) {
   return (
-    <div className="DictionaryMeaning rounded border border-gray mb-5 p-1">
-      <h3 className="bg-warning-subtle rounded"> {props.data.partOfSpeech}</h3>
-      <h4 className="bg-primary-subtle rounded">Definition</h4>
-      <p className="bg-light rounded">{props.data.definition}</p>
-      <h4 className="bg-primary-subtle rounded">Example</h4>
-      <p className="bg-light rounded">{props.data.example}</p>
+    <div>
+      <h3> {props.data.partOfSpeech}</h3>
+      <h4>Definition</h4>
+      <p>{props.data.definition}</p>
+      <h4>Example</h4>
+      <p>{props.data.example}</p>
     </div>
   );
 }
--- src/DisplayData.js
+++ src/DisplayData.js
@@ -6,10 +6,10 @@ export default function DisplayData(props) {
     console.log(props.data);
     return (
       <div className="DisplayData">
-        <h2 className="bg-danger rounded">{props.data.word}</h2>
-
+        <h2>{props.data.word}</h2>
+        <hr />
         {props.data.meanings.map(function (meaning, index) {
-          if (index < 3) {
+          if (index < 1) {
             return <DictionaryMeaning data={meaning} key={index} />;
           } else {
             return null;
